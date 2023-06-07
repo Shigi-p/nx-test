a7f827725273174a4ef6b6779460dea739c83b84
Shigi-p <shigi.522@gmail.com>
2023-06-07 13:06:09 +0900

    feat(apps/components/storybook): add storybook settings

--- apps/components/.storybook/preview.js
+++ apps/components/.storybook/preview.js
@@ -1,1 +1,41 @@
 import 'the-new-css-reset/css/reset.css';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  layout: 'fullscreen',
+  viewport: {
+    viewports: {
+      PC: {
+        name: 'PC',
+        styles: {
+          width: '1440px',
+          height: '810px',
+        },
+      },
+      SP: {
+        name: 'SP',
+        styles: {
+          width: '375px',
+          height: '667px',
+        },
+      },
+    },
+  },
+};
+
+const localeDecorator = (Story) => {
+  return (
+    <div id="__next">
+      <div id="rootInner">
+        <Story />
+      </div>
+    </div>
+  );
+};
+export const decorators = [localeDecorator];
